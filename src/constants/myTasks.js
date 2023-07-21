25d65a4303037502b81b9a754fa077164870d464
Antonije <antonijek@yahoo.com>
2023-07-21 09:26:38 +0200

    added keys in constants/tasks

--- src/constants/myTasks.js
+++ src/constants/myTasks.js
@@ -1,65 +1,77 @@
 export const allTasks = [
   {
+    key: "task-0",
     title: "Learn React Native",
     description:
       "Explore and gain knowledge of React Native framework for mobile app development",
     status: "Wish List",
   },
   {
+    key: "task-1",
     title: "Attend tech conference",
     description:
       "Participate in a technology conference to learn about the latest trends",
     status: "Wish List",
   },
   {
+    key: "task-2",
     title: "Improve project management skills",
     description: "Enhance project planning and organization abilities",
     status: "Wish List",
   },
   {
+    key: "task-3",
     title: "Finish coding feature A",
     description: "Complete the remaining tasks for implementing feature A",
     status: "To-Do List",
   },
   {
+    key: "task-4",
     title: "Review project requirements",
     description:
       "Go through the project requirements document and clarify any doubts",
     status: "To-Do List",
   },
   {
+    key: "task-5",
     title: "Prepare presentation slides",
     description:
       "Create a visually appealing slide deck for the project presentation",
     status: "To-Do List",
   },
   {
+    key: "task-6",
     title: "Debug issue with login functionality",
     description: "Investigate and fix the bug causing login failures",
     status: "In-Progress List",
   },
   {
+    key: "task-7",
     title: "Refactor code for better readability",
     description: "Simplify complex code blocks and improve code organization",
     status: "In-Progress List",
   },
   {
+    key: "task-8",
     title: "Write unit tests for module X",
     description:
       "Develop and execute unit tests to ensure the stability of module X",
     status: "In-Progress List",
   },
   {
+    key: "task-9",
     title: "Submit final report",
     description: "Review and submit the completed project report",
     status: "Done List",
   },
   {
+    key: "task-10",
     title: "Launch website",
     description: "Deploy the website to the production server",
     status: "Done List",
   },
   {
+    key: "task-11",
     title: "Deliver presentation",
     description: "Present the findings and recommendations to the stakeholders",
     status: "Done List",
